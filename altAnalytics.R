0743bf29c559d81de1820c41f5992ee2b3d7a063
lromang <luis.roangarci@gmail.com>
2017-01-11 14:08:44 -0600

    coords

--- altAnalytics.R
+++ altAnalytics.R
@@ -23,27 +23,22 @@ censo.filter <- censo[,c(8,9,10)]
 ## ---------------------------------
 ## Tesselate
 ## ---------------------------------
-grid     <- 40000                                    # Number of cells
+grid     <- 200000                                   # Number of cells
 tes      <- tesselate(grid,  map.plot, alpha = .05)  # Partition
 block    <- blocks(tes[[2]], tes[[3]])               # Cell creation
 
 ## Partition Denue
-cell_den <- in.block.fac(block,  denue)            # Cell characteristics
+cell_den <- in.block.fac(block,  denue.filter)       # Cell characteristics
 
 ## Partition Censo
-cell_cen <- in.block.fac(block, censo)            # Cell characteristics
-
-
-## Data population
-data_pop  <- in.block(block, data)
-allpop_1  <- laply(data_pop, function(t)t <- t[[3]])
+cell_cen <- in.block.fac(block, censo.filter)        # Cell characteristics
 
 ## ------------------------------
 ## Analysis
 ## ------------------------------
 
 ## Área por celda
-areatest_1 <- laply(cell_feat, function(t)t <- t[[4]])
+area_den <- laply(cell_den, function(t)t <- t[[4]])
 
 ## Observaciones por celda
-obs_1      <- laply(cell_feat, function(t)t <- nrow(t[[2]]))
+obs_den  <- laply(cell_den, function(t)t <- t[[3]])
