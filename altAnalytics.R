79adef27b700e35aef368a8d37a38e997be77635
lromang <luis.roangarci@gmail.com>
2017-01-11 13:29:00 -0600

    data clean

--- altAnalytics.R
+++ altAnalytics.R
@@ -13,17 +13,26 @@ map.plot  <- ggmap(map)
 ## ---------------------------------
 
 ## Denue
+denue        <- read.dbf("../data/denue/denue_front.dbf")
+denue.filter <- denue[,c(40, 39)]
 
+## Censo
+censo <- read.dbf("../data/censo/censo_front.dbf")
+censo.filter <- censo[,c(8,9,10)]
 
 ## ---------------------------------
 ## Tesselate
 ## ---------------------------------
+grid     <- 40000                                    # Number of cells
+tes      <- tesselate(grid,  map.plot, alpha = .05)  # Partition
+block    <- blocks(tes[[2]], tes[[3]])               # Cell creation
 
-## Partition
-grid      <- 40000                                    # Number of cells
-tes       <- tesselate(grid,  map.plot, alpha = .05)  # Partition
-block     <- blocks(tes[[2]], tes[[3]])               # Cell creation
-cell_feat <- in.block(block,  pts_denu_nl)            # Cell characteristics
+## Partition Denue
+cell_den <- in.block.fac(block,  denue)            # Cell characteristics
+
+## Partition Censo
+cell_cen <- in.block.fac(block, censo)            # Cell characteristics
+
 
 ## Data population
 data_pop  <- in.block(block, data)
